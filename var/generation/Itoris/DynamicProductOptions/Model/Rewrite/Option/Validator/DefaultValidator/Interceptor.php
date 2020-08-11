62def96b1b9e78aa0768d920b057497be6c19467
root <root@viper.syscon.ch>
2020-08-11 13:57:11 +0200

    removed var from gitignore

---
+++ var/generation/Itoris/DynamicProductOptions/Model/Rewrite/Option/Validator/DefaultValidator/Interceptor.php
@@ -0,0 +1,94 @@
+<?php
+namespace Itoris\DynamicProductOptions\Model\Rewrite\Option\Validator\DefaultValidator;
+
+/**
+ * Interceptor class for @see \Itoris\DynamicProductOptions\Model\Rewrite\Option\Validator\DefaultValidator
+ */
+class Interceptor extends \Itoris\DynamicProductOptions\Model\Rewrite\Option\Validator\DefaultValidator implements \Magento\Framework\Interception\InterceptorInterface
+{
+    use \Magento\Framework\Interception\Interceptor;
+
+    public function __construct(\Magento\Catalog\Model\ProductOptions\ConfigInterface $productOptionConfig, \Magento\Catalog\Model\Config\Source\Product\Options\Price $priceConfig)
+    {
+        $this->___init();
+        parent::__construct($productOptionConfig, $priceConfig);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isValid($value)
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'isValid');
+        if (!$pluginInfo) {
+            return parent::isValid($value);
+        } else {
+            return $this->___callPlugins('isValid', func_get_args(), $pluginInfo);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTranslator($translator = null)
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'setTranslator');
+        if (!$pluginInfo) {
+            return parent::setTranslator($translator);
+        } else {
+            return $this->___callPlugins('setTranslator', func_get_args(), $pluginInfo);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTranslator()
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'getTranslator');
+        if (!$pluginInfo) {
+            return parent::getTranslator();
+        } else {
+            return $this->___callPlugins('getTranslator', func_get_args(), $pluginInfo);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function hasTranslator()
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'hasTranslator');
+        if (!$pluginInfo) {
+            return parent::hasTranslator();
+        } else {
+            return $this->___callPlugins('hasTranslator', func_get_args(), $pluginInfo);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMessages()
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'getMessages');
+        if (!$pluginInfo) {
+            return parent::getMessages();
+        } else {
+            return $this->___callPlugins('getMessages', func_get_args(), $pluginInfo);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function hasMessages()
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'hasMessages');
+        if (!$pluginInfo) {
+            return parent::hasMessages();
+        } else {
+            return $this->___callPlugins('hasMessages', func_get_args(), $pluginInfo);
+        }
+    }
+}
---
+++ var/generation/Magento/Catalog/Model/Indexer/Product/Price/Action/Row/Interceptor.php
@@ -0,0 +1,42 @@
+<?php
+namespace Magento\Catalog\Model\Indexer\Product\Price\Action\Row;
+
+/**
+ * Interceptor class for @see \Magento\Catalog\Model\Indexer\Product\Price\Action\Row
+ */
+class Interceptor extends \Magento\Catalog\Model\Indexer\Product\Price\Action\Row implements \Magento\Framework\Interception\InterceptorInterface
+{
+    use \Magento\Framework\Interception\Interceptor;
+
+    public function __construct(\Magento\Framework\App\Config\ScopeConfigInterface $config, \Magento\Store\Model\StoreManagerInterface $storeManager, \Magento\Directory\Model\CurrencyFactory $currencyFactory, \Magento\Framework\Stdlib\DateTime\TimezoneInterface $localeDate, \Magento\Framework\Stdlib\DateTime $dateTime, \Magento\Catalog\Model\Product\Type $catalogProductType, \Magento\Catalog\Model\ResourceModel\Product\Indexer\Price\Factory $indexerPriceFactory, \Magento\Catalog\Model\ResourceModel\Product\Indexer\Price\DefaultPrice $defaultIndexerResource)
+    {
+        $this->___init();
+        parent::__construct($config, $storeManager, $currencyFactory, $localeDate, $dateTime, $catalogProductType, $indexerPriceFactory, $defaultIndexerResource);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute($id = null)
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'execute');
+        if (!$pluginInfo) {
+            return parent::execute($id);
+        } else {
+            return $this->___callPlugins('execute', func_get_args(), $pluginInfo);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTypeIndexers()
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'getTypeIndexers');
+        if (!$pluginInfo) {
+            return parent::getTypeIndexers();
+        } else {
+            return $this->___callPlugins('getTypeIndexers', func_get_args(), $pluginInfo);
+        }
+    }
+}
